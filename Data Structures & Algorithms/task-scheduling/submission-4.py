ffda092e7e44bc5f067adf6848a7873c8c49f571
Clara Sapugay <145624985+ctsapugay@users.noreply.github.com>
2026-08-20 11:52:17 -0700

    Add: task-scheduling - submission-4

---
+++ Data Structures & Algorithms/task-scheduling/submission-4.py
@@ -0,0 +1,39 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        time = 0
+
+        frequencies = {}
+        for t in tasks:
+            frequencies[t] = frequencies.get(t, 0) + 1
+
+        max_heap = []
+        for char, freq in frequencies.items():
+            heapq.heappush_max(max_heap, [freq, char])
+
+        cooldown = deque()
+        for i in range(n+1):
+            cooldown.append([-1, "-"])
+
+        cd_len = 0
+        while len(max_heap) > 0 or cd_len > 0:
+            # 1. process cool down item
+            item = cooldown.popleft()
+            if item[0] > 0:
+                heapq.heappush_max(max_heap, item)
+                cd_len -= 1
+
+        # process heap item, or empty, append idle time
+            if len(max_heap) > 0:
+                item = heapq.heappop_max(max_heap)
+                item[0] -= 1
+                if item[0] < 1:
+                    cooldown.append([-1, "-"]) 
+                else:
+                    cooldown.append(item)
+                    cd_len += 1 
+            else:
+                cooldown.append([-1, "-"]) 
+            
+            time += 1
+
+        return time
