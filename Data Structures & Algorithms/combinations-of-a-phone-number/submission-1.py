941f348f94865a2507f30302d171b846125a1f6b
Clara Sapugay <145624985+ctsapugay@users.noreply.github.com>
2026-09-01 15:10:15 -0700

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits == "": return []
+
+        translation = {
+            2 : "abc",
+            3 : "def",
+            4 : "ghi",
+            5 : "jkl",
+            6 : "mno",
+            7 : "pqrs",
+            8 : "tuv",
+            9 : "wxyz"
+        }
+
+        result = []
+        def dfs(pre, d):
+            nonlocal translation
+
+            # base case
+            if d >= len(digits):
+                # reached a leaf node
+                result.append("".join(pre))
+                return
+
+            for l in range(len(translation[int(digits[d])])):
+                # branch for each associated letter
+                pre.append(translation[int(digits[d])][l])
+                dfs(pre, d+1)
+                pre.pop()
+            
+            return
+        
+        dfs([], 0)
+        return result
+        
