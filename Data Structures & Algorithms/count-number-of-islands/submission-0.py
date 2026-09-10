286cb0e63a6dd84abece6b3355c0a7169e6750e3
Clara Sapugay <145624985+ctsapugay@users.noreply.github.com>
2026-09-10 10:16:08 -0700

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        result = 0
+        ROWS, COLS = len(grid), len(grid[0])
+
+        def dfs(r, c):
+            # given a node, if node is 1 check surrounding
+
+            # base case: water
+            if (r < 0 or c < 0 or
+                r >= ROWS or c >= COLS or
+                grid[r][c] == '0'): 
+                return
+
+            # 1 has been found
+            grid[r][c] = '0'
+            dfs(r-1, c)
+            dfs(r+1, c)
+            dfs(r, c-1)
+            dfs(r, c+1)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == '1':
+                    result += 1
+                    dfs(r, c)   
+
+        return result     
